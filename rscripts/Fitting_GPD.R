8ad2dac9ba8bb6560d4bc60812467911ca2d6b53
Anubhab17 <115042418+Anubhab17@users.noreply.github.com>
2024-07-05 15:28:35 +0200

    Update Fitting_GPD.R

--- rscripts/Fitting_GPD.R
+++ rscripts/Fitting_GPD.R
@@ -1,10 +1,5 @@
 library(evmix)
 
-data_folder <- "C:/Users/anubhab.biswas/OneDrive - SUPSI/Documenti/"
-D <- as.matrix(read.csv(paste0(data_folder,"STXGCN/Data/PM25_Daily_Data.csv")))
-result_folder <- paste0(data_folder,"STXGCN/results/")
-
-
 fit_pot_estimates <- function(train, u, name_month = "DEC", pollutant = "PM25"){
   stopifnot(is.matrix(train))
   stopifnot(is.character(name_month))
@@ -23,19 +18,7 @@ fit_pot_estimates <- function(train, u, name_month = "DEC", pollutant = "PM25"){
 }
 
 
-n_months <- 12
-names_of_months <- c("JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG",
-                     "SEP", "OCT", "NOV", "DEC")
-tr <- c(1441,1472,1500,1531,1561,1592,1622,1653,1684,1714,1745,1775)
-
-training_data <- list()
-
-for(k in seq(n_months)){
-  training_data[[k]] = D[1:tr[k],]
-}
-
 u_pm25 <- 60
 for(j in seq(n_months)){
   fit_pot_estimates(training_data[[j]], u_pm25, name_month = names_of_months[j], pollutant = "PM25")
 }
-
